156e4cc8e335a5e31271e98fd08a68519cdb2972
Adailton <paazadailton@gmail.com>
2026-06-27 00:23:29 -0300

    new creatios av2, adailton paz

--- app/src/main/java/com/example/av2pazadailton/ClassePessoa.kt
+++ app/src/main/java/com/example/av2pazadailton/ClassePessoa.kt
@@ -1,7 +1,20 @@
 package com.example.av2pazadailton
 
-data class ClassePessoa(
-    val nome: String,
-    val email: String,
-    val telefone: String
-)
+import java.io.Serializable
+
+
+
+class ClassePessoa : Serializable
+{
+    var nome: String = ""
+    var email: String = ""
+    var telefone: String =  ""
+    var favorito: Boolean = false
+
+    constructor(nome: String, email: String, telefone: String = "", favorito: Boolean = false){
+        this.nome = nome
+        this.email = email
+        this.telefone = telefone
+        this.favorito = favorito
+    }
+}
--- app/src/main/java/com/example/av2pazadailton/DB.kt
+++ app/src/main/java/com/example/av2pazadailton/DB.kt
@@ -1,4 +1,41 @@
 package com.example.av2pazadailton
 
-class DB {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+class DB (context: Context) :
+        SQLiteOpenHelper ( context, "db_av2", null, 4){
+    init{
+        Log.d("Adailton_av2", "DB-> init()")
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        Log.d("Adailton_av2", "DB->onCreate() = " + db.version.toString())
+
+        db.execSQL(
+            "CREATE TABLE usuario ( " +
+                "cod_usuario INTEGER PRIMARY KEY AUTOINCREMENT ," +
+                "nome_usuario TEXT, " +
+                "email_usuario TEXT, " +
+                "senha_usuario TEXT ); "
+        )
+        db.execSQL(
+            "CREATE TABLE clientes ( " +
+                "cod_cliente INTEGER PRIMARY KEY AUTOINCREMET, " +
+                "nome_cliente TEXT, " +
+                "email_cliente TEXT, " +
+                "telefone_cliente TEXT, " +
+                "favorito_cliente INTEGER );"
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        Log.d("Adailton_av2", "DB->onUpgrade() oldVersion:$oldVersion newVersion:$newVersion")
+        db.execSQL("DROP TABLE IF EXISTS usuario")
+        db.execSQL("DROP TABLE IF EXISTS clientes")
+        onCreate(db)
+
+    }
 }
